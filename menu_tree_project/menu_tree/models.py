ced1314610178cb04bad9caf4f89973037bde807
alapod <alapodrezova@gmail.com>
2023-01-06 23:36:01 +0500

    Added model

--- menu_tree_project/menu_tree/models.py
+++ menu_tree_project/menu_tree/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE)
--- menu_tree_project/menu_tree_project/urls.py
+++ menu_tree_project/menu_tree_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('menu_tree/', include('menu_tree.urls')),
     path('admin/', admin.site.urls),
